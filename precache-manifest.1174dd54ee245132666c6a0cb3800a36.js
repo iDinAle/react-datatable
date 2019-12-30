44f9c6f2bf4f46f1bd1e119f178d5cdd285f040e
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-30 18:17:29 +0200

    Updates

---
+++ precache-manifest.1174dd54ee245132666c6a0cb3800a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5a6f615cf89c27b2d22df3563130ac",
+    "url": "/react-datatable/index.html"
+  },
+  {
+    "revision": "09c0d4a1a1b5ee1d52df",
+    "url": "/react-datatable/static/css/main.81b24772.chunk.css"
+  },
+  {
+    "revision": "0ae0d190fcd4e977d9e1",
+    "url": "/react-datatable/static/js/2.8481a8ff.chunk.js"
+  },
+  {
+    "revision": "09c0d4a1a1b5ee1d52df",
+    "url": "/react-datatable/static/js/main.57c9a2f1.chunk.js"
+  },
+  {
+    "revision": "4a7f270f323948ec8a56",
+    "url": "/react-datatable/static/js/runtime~main.22478f67.js"
+  }
+]);
